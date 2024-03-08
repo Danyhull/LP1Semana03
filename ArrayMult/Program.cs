6e961dd4b1cb931a467ab0cef680ac43cd1e2a10
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-08 10:38:58 +0000

    Foi adicionado valores do tipo float

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float [,] matriz = new float [2,2];
+            float [] vetor = new float [2];
+            float [] resultado = new float [2];
+        }
+    }
+}
