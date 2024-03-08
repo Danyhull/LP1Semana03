a7d5be8cd4413c651bb32f77307828df477b60ec
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-08 12:01:01 +0000

    Foi criada o método dentro da classe Program e foi chamada

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        private static void PowerOfUntil5()
+            {
+                for (int i = 1, i <= (1 <<5); i = i <<1)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        static void Main(string[] args)
+        {
+            PowerOfUntil5();
+            PowerOfUntil5();
+        }
+
+    }
+}
