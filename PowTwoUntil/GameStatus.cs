26cfa6b1f6fbb5fe366cd513bff9331cb5b95cb8
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-08 18:09:49 +0000

    Foi criado o programa GameStatus

--- PowTwoUntil/GameStatus.cs
+++ PowTwoUntil/GameStatus.cs
@@ -1,23 +1,12 @@
 ﻿using System;
 
-namespace PowTwoUntil
+namespace RPS
 {
-    class Program
+    [Flags]
+    enum GameStatus
     {
-        private static void PowerOf2Until5()
-                {
-                    for (int i = 1; i <= (1<<5); i = i <<1)
-                    {
-                        Console.WriteLine(i);
-                    }
-                    
-                    
-                }
-        static void Main(string[] args)
-        {
-            PowerOf2Until5();
-            PowerOf2Until5();
-        }
-        
-    }
+        Draw = 0,
+        Player1Wins = 1,
+        Player2Wins = 2
+    };
 }
