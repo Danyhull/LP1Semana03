d68b2d172ede086e19210925798d6a30db953c7e
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-08 10:54:37 +0000

    Foi realizada uma multiplicação com um duplo ciclo for

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -9,6 +9,29 @@ namespace ArrayMult
             float [,] matriz = new float [2,2];
             float [] vetor = new float [2];
             float [] resultado = new float [2];
+
+            for (int i = 0; i < 2; i++)
+            {
+
+                for (int j = 0; j < 2; j++)
+                {
+                    matriz[i,j] =float.Parse(args[i*2 + j]);
+                }
+            }
+            for (int i = 0; i<2; i++)
+            {
+                vetor[i] = float.Parse(args [4+i]);
+            }
+            for (int i =  0; i < 2; i++)
+            {
+                for (int j = 0; j<2 ; j++)
+                {
+                    resultado[i] += matriz[i,j]*vetor[j];
+                }
+            }
+            Console.WriteLine(resultado[0]);
+            Console.WriteLine(resultado[1]);
+
         }
     }
 }
