a16c127015efef3d2d919c77511d3593d7d39ad6
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-08 10:07:10 +0000

    Foi criado um programa e testado que mostra no ecrã os argumentos da linha de comandos com comprimento superior a 3 caráteres.

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -6,7 +6,18 @@ namespace RightSize
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            foreach (string s in args)
+            {
+                if (s.Length > 3 && s.Length < 6)
+                {
+                    Console.WriteLine(s);
+                }
+                else if (s.Length >= 6)
+                {
+                    break;
+                }
+                
+            }
         }
     }
 }
